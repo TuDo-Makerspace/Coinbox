58a7de8cfc17b24746d633948a1dc4f796babef7
Patrick Pedersen <ctx.xda@gmail.com>
2025-05-21 20:44:03 +0200

    Use new sensor + Perform signal processing for better coin detection
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,23 @@
  * main.cpp
  *
  * Author       : Yunis, Patrick Pedersen
- * Description  : Main code for the coin box
+ * Description  : Mario Coin Box
+ *                A red-LED / photodiode pair monitors the coin slot and delivers
+ *                an analog signal to the MCU.  The firmware continually tracks a
+ *                running baseline via a low-pass filter, then looks for short-
+ *                duration spikes above that baseline to identify passing coins.
+ *                Each coin increments an internal counter and triggers the
+ *                appropriate Mario sound effect (coin, power-up, 1-up, …)
+ *                according to how many coins have been collected so far.
  *
+ * 			  	  Measurements of the coin slot signal can be found in the measurements
+ *				  directory of this repository.
  */
 
 #include <Arduino.h>
 #include <PCM.h>
 
 #include "sounds.h"
-
-//// Parameters
 
 // Pins
 // Sound is on pin 11
@@ -24,74 +31,112 @@
 // Function
 #define WAIT_AFTER_COIN 100 // ms
 
-/* From: https://docs.arduino.cc/language-reference/en/functions/digital-io/digitalread/:
- *   The analog input pins can be used as digital pins, referred to as A0, A1, etc.
- *   The exception is the Arduino Nano, Arduino Pro Mini, and Arduino Mini’s A6 and A7 pins,
- *   which can only be used as analog inputs. */
-#if SENSOR_PIN == A6 || SENSOR_PIN == A7
-    #define SENSOR_THRESHOLD 512
-    #define SENSOR_READ() (analogRead(SENSOR_PIN) > SENSOR_THRESHOLD ? HIGH : LOW)
-#else
-    #define SENSOR_READ() digitalRead(SENSOR_PIN)
-#endif
+// Filter parameters
+#define BASELINE_ALPHA 0.02f  	// 0 - 1  (smaller = slower baseline)
+#define SPIKE_THRESHOLD 100   	// ADC counts above baseline
+#define SPIKE_MAX_MS 100      	// must fall back within this time
+#define SAMPLE_PERIOD_US 2000 	// ~500 Hz sampling
+#define WAIT_AFTER_COIN 100 	// coolwdown time after coin detection
+
+// State machine
+enum State {
+    IDLE,
+    SPIKE_START,
+    SPIKE_END,
+};
 
 void print_boot_msg() {
-  Serial.println("@@@@/////////////////////////////////////////////////@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,/////////////////*,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,/////////////////*,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,///////@@@@@@@@@@@///////,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,@@@@@@@,,,///////////@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@@@@@@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@@@@@@@@@,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@#,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////,,,,,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////,,,,,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@#,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,@@@@,,,@@@@\n");
-  Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,@@@@,,,@@@@\n");
-  Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
-  Serial.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
-  Serial.println("\n\n");
-  Serial.println("Mario Coin box by Yunis! ft. Patrick :) !!!!11!\n");
+    Serial.println("@@@@/////////////////////////////////////////////////@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,/////////////////*,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,/////////////////*,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,///////@@@@@@@@@@@///////,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,///////@@@@,,,,,,,///////@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,@@@@@@@,,,///////////@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@@@@@@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@@@@@@@@@,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@#,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////,,,,,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////,,,,,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,///////@@@#,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,@@@@,,,@@@@\n");
+    Serial.println("////,,,@@@@,,,,,,,,,,,,,,,,,@@@@@@@#,,,,,,,,,,@@@@,,,@@@@\n");
+    Serial.println("////,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@\n");
+    Serial.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    Serial.println("\n\n");
+    Serial.println("Mario Coin box by Yunis! ft. Patrick :) !!!!11!\n");
 }
 
-int money = 1;
-
 void setup() {
-  pinMode(SENSOR_PIN, INPUT);
-  Serial.begin(9600);
-  print_boot_msg();
+    pinMode(SENSOR_PIN, INPUT);
+    Serial.begin(9600);
+    print_boot_msg();
 }
 
 void loop() {
-  static uint8_t last_val = SENSOR_READ();
-  uint8_t val = SENSOR_READ();
+    // Runtime variables
+    static float baseline = 0;         		// running baseline (float for filter)
+    static bool baseline_init = false; 		// first-run flag
+    static int money = 1;              		// counter for sounds
+    static State     state         = IDLE;	// state machine
+    static uint32_t  spike_start   = 0;		// time of spike start
+    static uint32_t  last_coin_ms  = 0;		// time of last coin detection
 
-  if (last_val == LOW && val == HIGH) {
+    uint16_t raw = analogRead(SENSOR_PIN);
 
-    Serial.print("Coin detected! Playing: ");
-
-    if ((money % POWER_UP) == 0) {
-      Serial.println("Power up!");
-      startPlayback(powerup, sizeof(powerup));
-    } else if ((money % ONE_UP) == 0) {
-      Serial.println("One up!");
-      startPlayback(oneup, sizeof(oneup));
-    } else {
-      Serial.println("Coin sound!");
-      startPlayback(coin, sizeof(coin));
+    if (!baseline_init) {
+        baseline = raw;
+        baseline_init = true;
     }
-    money++;
 
-    delay(WAIT_AFTER_COIN);
-  }
+    // Deviation from baseline
+    int16_t diff = (int16_t)raw - (int16_t)baseline;
 
-  last_val = val;
+    switch (state) {
+    case IDLE:
+        if (abs(diff) > SPIKE_THRESHOLD) {
+            state       = SPIKE_START;
+            spike_start = millis();
+        }
+        break;
+
+    case SPIKE_START:
+        if (abs(diff) < SPIKE_THRESHOLD) {
+            state = SPIKE_END;                     // Spike ended, this is a coin!
+        } else if ((millis() - spike_start) > SPIKE_MAX_MS) {
+            state    = IDLE;                       // Probably just a change in light, not a coin...
+            baseline = raw;
+        }
+        break;
+
+    case SPIKE_END:
+        if ((millis() - last_coin_ms) > WAIT_AFTER_COIN) { // cooldown
+            Serial.println(F("Coin detected!"));
+            if ((money % POWER_UP) == 0) {
+                Serial.println(F("Power-up!"));
+                startPlayback(powerup, sizeof(powerup));
+            } else if ((money % ONE_UP) == 0) {
+                Serial.println(F("One-up!"));
+                startPlayback(oneup, sizeof(oneup));
+            } else {
+                Serial.println(F("Coin sound!"));
+                startPlayback(coin, sizeof(coin));
+            }
+            money++;
+            last_coin_ms = millis();
+        }
+        state = IDLE;
+        break;
+    }
+
+    if (state == IDLE) {
+        baseline += BASELINE_ALPHA * ((float)raw - baseline);
+    }
+
+    delayMicroseconds(SAMPLE_PERIOD_US);
 }
